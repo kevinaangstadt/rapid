bae5598385e21f1a6b935d78b89459a7fda70f26
Kevin Angstadt <kaa2nx@virginia.edu>
2015-03-27 10:16:21 -0400

    additions to the test suite

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-array=("tst" "tst2" "string_kernels" "frequent")
+array=("tst" "tst2" "tst3" "tst4" "tst5" "tst6" "hamming" "string_kernels" "frequent")
 for i in "${array[@]}"; do   # The quotes are necessary here
     echo "=========== $i ============="
     ./rapidsim -input test/$i.txt test/$i.ap
